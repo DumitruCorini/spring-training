68c62658995561d6340dca6f94e5b9fc8e869461
DumitruCorini <dumitru.corini@gmail.com>
2022-12-09 15:20:05 +0100

    Finish authors/books

--- Section 2/src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ Section 2/src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -91,7 +91,6 @@ public class Book {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", authors=" + authors +
                 '}';
     }
 }
